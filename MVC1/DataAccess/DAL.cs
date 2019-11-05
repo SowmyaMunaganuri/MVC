84c6c001d1c39caf6b7215b866c1f93bd5597965
Sowmya Munaganuri <sowmya.munaganuri@gmail.com>
2019-11-05 16:59:09 -0500

    First

---
+++ MVC1/DataAccess/DAL.cs
@@ -0,0 +1,24 @@
+﻿using MVC1.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC1.DataAccess
+{
+    public class DAL
+    {
+        PASSEntities db = new PASSEntities();
+
+        public List<Employee> GetEmployees()
+        {
+            return db.Employees.Where(x=>x.isActive == "Y").Take(20).ToList();
+        }
+
+        public Employee GetEmployeeById(long id)
+        {
+            return db.Employees.Where(x => x.employeeId == id).SingleOrDefault();
+        }
+
+    }
+}
